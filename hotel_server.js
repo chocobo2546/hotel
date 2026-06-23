88d54fe112d7e10ec25b5e04641f1c4f6f15e874
Chocobo <pattrawutkeowkom@gmail.com>
2026-06-23 16:38:22 +0700

    add deployment config and cors support

--- hotel_server.js
+++ hotel_server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
